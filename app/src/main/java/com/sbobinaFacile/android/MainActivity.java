1d2cf381665165afbdbee982e887bf2105632715
gero <f.calogero@hotmail.it>
2018-10-26 18:21:08 +0200

    Aggiunte --Layout principale di test --Main Activity --XML delle stringhe

--- app/src/main/java/com/sbobinaFacile/android/MainActivity.java
+++ app/src/main/java/com/sbobinaFacile/android/MainActivity.java
@@ -1,7 +1,17 @@
 package com.sbobinaFacile.android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.sbobinaFacile.R;
+import com.sbobinaFacile.speechRecognition.SpeechListener;
+
 
 /**
  * Activity principale dell'app
@@ -10,12 +20,51 @@ import android.support.v7.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
+    //      COSTANTI
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static final String TAG = "Activity_Main";
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //      CAMPI
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private SpeechRecognizer recognizer;
+    private Button buttonStart, buttonStop;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     //      SOVRACCARICO METODI DELLA GESTIONE DEL CICLO DI VITA
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recognizer = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+        recognizer.setRecognitionListener(new SpeechListener());
+
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStop = (Button) findViewById(R.id.buttonStop);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                if(recognizer != null){
+                    Log.v(TAG, "Premuto pulsante di inizio registrazione");
+                    //TODO : Start recognition
+                }
+            }
+        });
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                if(recognizer != null){
+                    Log.v(TAG, "Premuto pulsante di fine registrazione");
+                    //TODO : Stop recognition and visualize results
+                }
+            }
+        });
+
     }
 
     @Override
